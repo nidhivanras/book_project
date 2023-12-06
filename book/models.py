392e004560dad3d86a09b0af4e80a0fb94c96638
sanjay <sanjaypython22@gmail.com>
2023-12-06 21:44:23 +0530

    iouytre

---
+++ book/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+# Create your models here.
+class Writer(models.Model):
+    name = models.CharField(max_length=256)
+    desc = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='writer_image')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Book(models.Model):
+    name = models.TextField()
+    image = models.ImageField(upload_to='book_image')
+    writer = models.ForeignKey(Writer, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(null=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class BookIndex(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    index_title = models.TextField()
+    index_no = models.IntegerField()
+    file = models.FileField(upload_to='book_index_pdfs')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(null=True)
+
+    def __str__(self):
+        return str(str(self.book.name) + str(self.index_title))
---
+++ vichar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VicharConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vichar'
